8839f7931f552b4fe87ec05a8e4a81d118f42f3e
vaskrneup <bhaskar.neupane.58@gmail.com>
2021-01-12 14:45:36 +0545

    Array transform function complete !!

---
+++ js/transform.js
@@ -0,0 +1,19 @@
+let numbers = [1, 2, 3, 4];
+
+function transform(collection, tranFunc) {
+    const transformedNumberCollection = [];
+
+    for (let i = 0; i < collection.length; i++) {
+        const transformedNumber = tranFunc(collection[i]);
+
+        transformedNumberCollection.push(transformedNumber);
+    }
+
+    return transformedNumberCollection;
+}
+
+let output = transform(numbers, function (num) {
+    return num * 2;
+});
+
+console.log(output)
