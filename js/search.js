ec7bad0f670df9cf465f405ef12aa3fe2aa9fa05
vaskrneup <bhaskar.neupane.58@gmail.com>
2021-01-12 14:39:54 +0545

    Search Assignment Complete !!

---
+++ js/search.js
@@ -0,0 +1,26 @@
+const fruits = [
+    {id: 1, name: 'Banana', color: 'Yellow'},
+    {id: 2, name: 'Apple', color: 'Red'}
+];
+
+function searchByKey(data, key, value) {
+    value = value.toLowerCase();
+
+    for (let i = 0; i < data.length; i++) {
+        const _data = data[i];
+
+        if (_data[key].toLowerCase() === value) {
+            return _data;
+        }
+    }
+
+    return undefined;
+}
+
+function searchByName(data, name) {
+    return searchByKey(data, "name", name);
+}
+
+
+console.log(searchByName(fruits, "apple"))
+console.log(searchByKey(fruits, "name", "apple"))
