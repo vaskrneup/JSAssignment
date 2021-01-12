8b8f4ddff6c2f79fae8a56bb87eff537c0038c00
vaskrneup <bhaskar.neupane.58@gmail.com>
2021-01-12 15:26:11 +0545

    Array sorting algo. complete !!

---
+++ js/sorting.js
@@ -0,0 +1,41 @@
+let arr = [{
+    id: 1,
+    name: 'John',
+}, {
+    id: 2,
+    name: 'Mary',
+}, {
+    id: 3,
+    name: 'Andrew',
+}];
+
+// [6,5,4,3,2,1]
+
+function sortBy(array, key, asc = true) {
+    const sortedArray = array.slice();
+
+    for (let i = 0; i < sortedArray.length; i++) {
+        let sorted = true;
+
+        for (let j = 0; j < (sortedArray.length - 1); j++) {
+            const first = sortedArray[j];
+            const sec = sortedArray[j + 1];
+
+            if (asc ? first[key] > sec[key] : first[key] < sec[key]) {
+                sortedArray[j] = sec;
+                sortedArray[j + 1] = first;
+
+                sorted = false;
+            }
+        }
+
+        if (sorted) {
+            break
+        }
+    }
+
+    return sortedArray;
+}
+
+let sorted = sortBy(arr, 'name');
+console.log(sorted)
