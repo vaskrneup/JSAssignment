1f1f0a13df159252063419017878c3c528d852d9
vaskrneup <bhaskar.neupane.58@gmail.com>
2021-01-13 19:12:15 +0545

    plotter completed !!

---
+++ graph_and_bouncy_ball/js/main.js
@@ -0,0 +1,36 @@
+const points = [
+    {x: 10, y: 20},
+    {x: 40, y: 40},
+    {x: 60, y: 20},
+];
+
+const canvas = document.getElementById("bouncy-ball");
+const pointSize = 4;
+
+function handlePointRemoval(e) {
+    canvas.removeChild(e.target);
+}
+
+
+points.forEach(function (value) {
+    const ball = document.createElement("div");
+    ball.style.width = pointSize + "px";
+    ball.style.height = pointSize + "px";
+    ball.style.backgroundColor = "lightblue";
+    ball.style.borderRadius = "50%";
+    ball.style.position = "absolute";
+    ball.style.left = (value.x * pointSize) + "px";
+    ball.style.top = (value.y * pointSize) + "px";
+
+    canvas.appendChild(ball);
+
+    ball.addEventListener("click", handlePointRemoval);
+    ball.addEventListener("mouseenter", (e) => {
+        ball.style.backgroundColor = "red";
+    });
+    ball.addEventListener("mouseleave", (e) => {
+        ball.style.backgroundColor = "lightblue";
+    });
+
+    return ball;
+});
