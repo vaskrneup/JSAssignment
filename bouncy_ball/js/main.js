84433ccbf06185d3ead9f34fa0bfbb1cd33fd821
vaskrneup <bhaskar.neupane.58@gmail.com>
2021-01-13 20:00:43 +0545

    Jumping ball completed !!

--- bouncy_ball/js/main.js
+++ bouncy_ball/js/main.js
@@ -1,1 +1,39 @@
 const canvas = document.getElementById("bouncy-ball");
+const ball = document.createElement("div");
+const canvasHeight = 800;
+const ballHeight = 90;
+
+let fall = false;
+let currentHeight = 0;
+let s = 10;
+
+ball.style.width = "90px";
+ball.style.height = "90px";
+ball.style.borderRadius = "50%";
+
+ball.style.left = "205px";
+ball.style.position = "absolute";
+ball.style.backgroundColor = "black";
+
+canvas.appendChild(ball)
+
+
+function animateBall() {
+    if (currentHeight <= 0 || currentHeight >= (canvasHeight - ballHeight)) {
+        fall = !fall;
+    }
+
+    if (fall) {
+        currentHeight += s;
+    } else {
+        currentHeight -= s;
+    }
+
+    ball.style.top = currentHeight;
+
+    ball.style.top = currentHeight + "px";
+
+    requestAnimationFrame(animateBall);
+}
+
+animateBall();
